dd36c7f7aa97a5757ce9c899a21605aea71f0d2f
AdiRoH <adididi149@gmail.com>
2022-04-11 20:24:05 +0300

    first uploading

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Login from './/assets/pages/Login/Login'
+import Dashboard from './/assets/Dashboard/Dashboard';
+import Preferences from './/assets/Preferences/Preferences';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [token, setToken] = useState();
+    if (!token) {
+        return <Login setToken={setToken} />
+    }
+    return (
+        <div className="wrapper">
+            <h1>Application</h1>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/dashboard" element={<Dashboard/>}></Route>
+                    <Route path="/preferences" element={<Preferences/>}></Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+
+  
 }
 
 export default App;
---
+++ src/Routes.js
@@ -0,0 +1,7 @@
+import Login from './/assets/pages/Login/Login';
+//import Dashboard from './/assets/Dashboard/Dashboard';
+<Route exact path="/login">
+
+	<Login />
+
+</Route>
