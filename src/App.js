8049b25df9adef1ddbff4441c11c6cc2ab1896e7
orhalimi <or50002001@gmail.com>
2022-05-23 22:41:53 +0300

    lesson-example

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
-import React from 'react';
-import './App.css';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import Login from './/components/pages/Login/Login'
-import Dashboard from './/components/Dashboard/Dashboard';
-import Preferences from './/components/Preferences/Preferences';
-import useToken from './/components/App/useToken';
-
-
-
+import React from "react";
+import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Login from ".//components/pages/Login/Login";
+import Dashboard from ".//components/Dashboard/Dashboard";
+import Preferences from ".//components/Preferences/Preferences";
+import useToken from ".//components/App/useToken";
+import { height } from "@mui/system";
 
 function App() {
-    const { token, setToken } = useToken();
-    if (!token) {
-        return <Login setToken={setToken} />
-    }
-    return (
-        <div className="wrapper">
-            <h1>Application</h1>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/dashboard" element={<Dashboard/>}></Route>
-                    <Route path="/preferences" element={<Preferences/>}></Route>
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
+  const { token, setToken } = useToken();
 
-  
+  return (
+    <div
+      className="wrapper"
+      style={{
+        backgroundImage:
+          'url("/static/media/background.910f6fc98a64c80ce1d2.PNG")',
+        height: "100vh",
+      }}
+    >
+      <BrowserRouter>
+        <Routes>
+          {!token ? (
+            <Route path="/" element={<Login setToken={setToken} />}></Route>
+          ) : (
+            <>
+              <Route path="/dashboard" element={<Dashboard />}></Route>
+              <Route path="/preferences" element={<Preferences />}></Route>
+            </>
+          )}
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App; 
+export default App;
